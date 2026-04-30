04152390f161d444f7df58d4d7a7893866a18029
Alejandro Solorio <a.solorioresendiz1@universityofgalway.ie>
2026-04-30 23:19:57 +0100

    Add Python build dependencies for pyenv

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -14,3 +14,18 @@ apt install -y \
   sox \
   libsndfile1 \
   curl
+
+sudo apt install -y \
+  build-essential \
+  libssl-dev \
+  zlib1g-dev \
+  libbz2-dev \
+  libreadline-dev \
+  libsqlite3-dev \
+  libffi-dev \
+  libncursesw5-dev \
+  xz-utils \
+  tk-dev \
+  libxml2-dev \
+  libxmlsec1-dev \
+  liblzma-dev
